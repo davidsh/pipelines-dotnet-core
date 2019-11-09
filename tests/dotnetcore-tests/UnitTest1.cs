64e0e555ea7bbb4a6379130828102cdfaa486585
David Shulman <david.shulman@microsoft.com>
2019-11-09 09:14:49 -0800

    Add more tests

--- tests/dotnetcore-tests/UnitTest1.cs
+++ tests/dotnetcore-tests/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace dotnetcore_tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private ITestOutputHelper _output;
 
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void PassingTest()
+        {
+            _output.WriteLine("PassingTest");
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            _output.WriteLine("FailingTest");
+            int i = 5;
+            Assert.Equal(4, i);
         }
     }
 }
